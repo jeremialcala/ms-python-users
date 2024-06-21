eafd4354102299114c014e2370c4a1f6ab04eec6
Jeremi Alcala <jeremialcala@gmail.com>
2024-06-21 08:17:53 -0400

    Adding the logging configuration

---
+++ utils/__init__.py
@@ -0,0 +1,5 @@
+"""
+    This module have utilities methods for this project
+"""
+
+from .logging import configure_logging
---
+++ utils/logging.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+    This is an interface for logging utilities.
+"""
+import yaml
+
+
+def configure_logging():
+    """
+        This method generates a logging configuration using a yaml file.
+    :return: this config variable have an assortment of configurations to be used by logging.config.
+    """
+    with open('logging_config.yaml', 'rt') as f:
+        config = yaml.safe_load(f.read())
+    return config

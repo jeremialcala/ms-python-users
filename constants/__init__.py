bc53d7d51a64d7b4855795f746c17cb0ced296d0
Jeremi Alcala <jeremialcala@gmail.com>
2024-06-22 23:51:24 -0400

    Adding constants for general messages

---
+++ constants/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+""""
+    The module that store constants for this project
+"""
+from .general import STARTING_AT, ENDING_AT
---
+++ constants/general.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+    This is the constants for general use
+"""
+STARTING_AT = "Starting: %s"
+ENDING_AT = "Ending: %s"
